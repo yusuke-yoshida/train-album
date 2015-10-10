d3999f865498ad8a7faf664561a2d2e99a1d03b3
yusuke_yoshida <yoshidy37@gmail.com>
2015-10-10 15:32:55 +0000

    add user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,27 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = "プロフィールを更新できました！"
+      redirect_to user_path
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
   
 end
