6e34c7467110055bff6121e63aa1d06704f33988
yusuke_yoshida <yoshidy37@gmail.com>
2015-10-10 14:28:21 +0000

    add signup and user detail

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
   root 'welcome#index'
 
+  get 'signup', to: 'users#new'
+  
+  resources :users
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
