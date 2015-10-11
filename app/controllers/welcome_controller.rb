3868719b7bbd9cb120ca11f2a8dcc33d0d79972e
yusuke_yoshida <yoshidy37@gmail.com>
2015-10-11 15:37:06 +0000

    changed index.html.erb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    FlickRaw.api_key       = Rails.application.secrets.flickr_api_key
-    FlickRaw.shared_secret = Rails.application.secrets.flickr_shared_secret
-    word = "はやぶさ"
-    @photos = flickr.photos.search(text: word, license: "1,2,3,4,5,6", sort: "relevance", per_page: 30)
+    @photos = Photo.all.order("updated_at DESC").limit(30)
   end
 end
