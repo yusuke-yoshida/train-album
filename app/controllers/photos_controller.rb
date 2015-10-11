df29b15388cbb5d505411bf30b788ed74103dab6
yusuke_yoshida <yoshidy37@gmail.com>
2015-10-11 00:54:38 +0000

    add photo new(search) controller and view

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,11 +1,12 @@
 class PhotosController < ApplicationController
-    before_action :logged_in_user , except: [:show]
     
-    def flickr_search(word)
-        FlickRaw.api_key       = Rails.application.secrets.flickr_api_key
-        FlickRaw.shared_secret = Rails.application.secrets.flickr_shared_secret
-        word = "はやぶさ"
-        @photos = flickr.photos.search(text: word, license: "1,2,3,4,5,6", sort: "relevance", per_page: 30)
+    def new
+        if params[:q]
+            FlickRaw.api_key       = Rails.application.secrets.flickr_api_key
+            FlickRaw.shared_secret = Rails.application.secrets.flickr_shared_secret
+            word = params[:q]
+            @photos = flickr.photos.search(text: word, license: "1,2,3,4,5,6", sort: "relevance", per_page: 30)
+        end
     end
     
     def show
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :photos, only: [:new, :show]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
